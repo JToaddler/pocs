99738b09809c8d3b501cc12018a01a76af28bafa
Arock <Arock@host.docker.internal>
2022-04-15 20:40:44 +0530

    Init Gradle library commit

---
+++ gradle/fms-api/src/main/java/com/example/demo/config/ServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceTest {
+
+	public void test() {
+		System.out.println("Test Service");
+	}
+
+}
